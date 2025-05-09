0f40b2ff318412d49bbeb63116664a404040bc93
dario <dario43055@gmail.com>
2025-05-08 22:40:18 -0300

    clase 3

---
+++ CLASES4.js
@@ -0,0 +1,30 @@
+class Producto {
+    constructor (n, p, s){
+        this.nombre = n;
+        this.precio = p;
+        this.stock = s
+
+    }
+
+    info(){
+        return `El nombre del producto es: ${this.nombre} su precio es: ${this.precio} y el stock: es ${this.stock}`
+        }
+
+        sumarAlStock(prod){
+        this.stock += prod
+        }
+
+        restarAlStock(prod){
+            this.stock -= prod
+            }
+
+}
+
+let producto1 = new Producto('iphone', '500usd', '20');
+
+console.log(producto1.info())
+producto1.restarAlStock(2)
+console.log(producto1.info())
+producto1.sumarAlStock(400)
+console.log(`STOCK ACTUALIZADO: ${producto1.stock}`)
+    
